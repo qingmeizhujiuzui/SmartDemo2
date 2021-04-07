141458d40aeb6768b308561408f6cd049c7b49b7
caiqing <1093374690@qq.com>
2021-04-08 01:03:59 +0800

    1.修改teacher

---
+++ src/test/java/com/demo/Teacher.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+/**
+ * @Auther.caiqing
+ * @Date:2021/4/8 0008-04-08-上午 12:59
+ * @Description:com.demo
+ * @version:1.0
+ */
+public class Teacher {
+    public static void main(String[] args) {
+        System.out.println("teacher test");
+    }
+}
