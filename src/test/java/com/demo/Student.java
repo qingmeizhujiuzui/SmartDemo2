73319fc67c6fea59e43bb306f0545c9509a437e8
caiqing <1093374690@qq.com>
2021-04-08 00:53:30 +0800

    commit SmartDemo2

---
+++ src/test/java/com/demo/Student.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import java.io.Serializable;
+
+/**
+ * @Auther.caiqing
+ * @Date:2021/4/7 0007-04-07-下午 4:38
+ * @Description:com.demo
+ * @version:1.0
+ */
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = -8372449672507818217L;
+}
